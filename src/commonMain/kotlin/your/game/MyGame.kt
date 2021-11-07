00bfeba2d91cf3be92503de57f6d6811a7471b09
David Wursteisen <david.wursteisen+git@gmail.com>
2021-11-07 11:17:18 +0100

    Init

--- src/commonMain/kotlin/your/game/MyGame.kt
+++ src/commonMain/kotlin/your/game/MyGame.kt
@@ -1,6 +1,5 @@
 package your.game
 
-import com.dwursteisen.minigdx.scene.api.Scene
 import com.dwursteisen.minigdx.scene.api.relation.ObjectType
 import com.github.dwursteisen.minigdx.GameContext
 import com.github.dwursteisen.minigdx.ecs.Engine
@@ -8,22 +7,24 @@ import com.github.dwursteisen.minigdx.ecs.entities.EntityFactory
 import com.github.dwursteisen.minigdx.ecs.systems.System
 import com.github.dwursteisen.minigdx.file.get
 import com.github.dwursteisen.minigdx.game.Game
+import com.github.dwursteisen.minigdx.graph.GraphScene
 import your.game.components.Cube
 import your.game.systems.RotatingCubeSystem
 
 class MyGame(override val gameContext: GameContext) : Game {
 
-    private val scene by gameContext.fileHandler.get<Scene>("cube.protobuf")
+    @OptIn(ExperimentalStdlibApi::class)
+    private val scene by gameContext.fileHandler.get<GraphScene>("cube.protobuf")
 
     override fun createEntities(entityFactory: EntityFactory) {
         // Create all entities needed at startup
         // The scene is the node graph that can be updated in Blender
-        scene.children.forEach { node ->
+        scene.nodes.forEach { node ->
             // Create an entity using all information from this node (model, position, camera, ...)
-            val entity = entityFactory.createFromNode(node, scene)
+            val entity = entityFactory.createFromNode(node)
 
             // The node is the cube from the Blender file
-            if(node.type == ObjectType.MODEL) {
+            if (node.type == ObjectType.MODEL) {
                 // Mark this entity as being a cube
                 entity.add(Cube())
             }
